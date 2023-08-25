d24a772dda4fd269b9cbf1dfac0febaadc35b2a8
LakshanaK17 <lakshanak902@gmail.com>
2023-08-25 08:27:27 +0530

    Updated Frontend

--- demo/src/routes/Header.svelte
+++ demo/src/routes/Header.svelte
@@ -1,4 +1,4 @@
-<!-- <script>
+<script>
 	import { page } from '$app/stores';
 	import logo from '$lib/images/svelte-logo.svg';
 	import github from '$lib/images/github.svg';
@@ -16,16 +16,16 @@
 			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
 		</svg>
 		<ul>
-			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
+			<!-- <li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
 				<a href="/">Home</a>
-			</li>
+			</li> -->
 			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
 				<a href="/about">About</a>
 			</li>
-			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
+			<!-- <li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
 				<a href="/sverdle">Sverdle</a>
 			</li>
-		</ul>
+		</ul> -->
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
 		</svg>
@@ -126,4 +126,4 @@
 	a:hover {
 		color: var(--color-theme-1);
 	}
-</style> -->
+</style>
